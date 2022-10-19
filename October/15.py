ee4fc3749b9968519f85a137e406e59e24cb0b00
shubham bakre <shubhambakre@gmail.com>
2022-10-19 03:29:40 -0700

    DCP

--- October/15.py
+++ October/15.py
@@ -1,16 +1,41 @@
-#
-# Good morning! Here's your coding interview problem for today.
-#
-# This problem was asked by Microsoft.
-#
-# You are given an string representing the initial conditions of some dominoes. Each element can take one of three values:
-#
-# L, meaning the domino has just been pushed to the left,
-# R, meaning the domino has just been pushed to the right, or
-# ., meaning the domino is standing still.
-# Determine the orientation of each tile when the dominoes stop falling. Note that if a domino receives a force from the left and right side simultaneously, it will remain upright.
-#
-# For example, given the string .L.R....L, you should return LL.RRRLLL.
-#
-# Given the string ..R...L.L, you should return ..RR.LLLL.
-#
+"""
+Problem:
+
+In front of you is a row of N coins, with values v_1, v_2, ..., v_n.
+
+You are asked to play the following game. You and an opponent take turns choosing
+either the first or last coin from the row, removing it from the row, and receiving the
+value of the coin.
+
+Write a program that returns the maximum amount of money you can win with certainty, if
+you move first, assuming your opponent plays optimally.
+"""
+
+from typing import List
+
+
+def optimal_strategy_of_game(
+    coins_arr: List[int], amount: int = 0, to_play: bool = True
+) -> int:
+    if not coins_arr:
+        return amount
+    if to_play:
+        return max(
+            optimal_strategy_of_game(coins_arr[1:], amount + coins_arr[0], False),
+            optimal_strategy_of_game(coins_arr[:-1], amount + coins_arr[-1], False),
+        )
+    if coins_arr[0] > coins_arr[-1]:
+        return optimal_strategy_of_game(coins_arr[1:], amount, True)
+    return optimal_strategy_of_game(coins_arr[:-1], amount, True)
+
+
+if __name__ == "__main__":
+    print(optimal_strategy_of_game([1, 2, 3, 4, 5]))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(2 ^ n)
+SPACE COMPLEXITY: O(n)
+"""
