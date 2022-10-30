8d4464a19abd5aa1ef396e1068d964166094ee62
Shubham <shubhambakre@gmail.com>
2022-10-30 14:05:27 -0700

    Update 21.py

--- October/21.py
+++ October/21.py
@@ -1,3 +1,11 @@
 # This problem was asked by Apple.
 #
 # Implement the function fib(n), which returns the nth number in the Fibonacci sequence, using only O(1) space.
+
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        return self.fib(n-1) + self.fib(n-2)
